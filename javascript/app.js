99bda3fe6bfed0a7a5b67e6fa597502ec23b57de
Alan Perez <alanipce@gmail.com>
2015-08-16 22:01:41 -0700

    Setup basic page routing

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,36 @@
-var app = angular.module('website-me', ['ngAnimate', 'ui.bootstrap']);
-		
-app.controller('navController', function ($scope) {
+var app = angular.module('website-me', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
+
+app.config(function ($routeProvider) {
+	$routeProvider.
+      when('/About', {
+        templateUrl: 'partials/About.html'
+      }).
+      when('/Projects', {
+        templateUrl: 'partials/Projects.html'
+      }).
+      when('/Career', {
+        templateUrl: 'partials/Career.html'
+      }).
+      when('/Resume', {
+        templateUrl: 'partials/Resume.html'
+      }).
+      when('/Contact', {
+        templateUrl: 'partials/Contact.html'
+      }).
+      otherwise({
+        redirectTo: '/About'
+      });
+});
+
+app.controller('navController', function ($scope, $location) {
 	$scope.isCollapsed = true;
 	console.log("angularjs is up");
+
+	$scope.isActive = function (viewLocation) {
+		return $location.path().indexOf(viewLocation) === 0;
+	};
+
+	$scope.itemSelected = function () {
+		$scope.isCollapsed = true;
+	};
 });
